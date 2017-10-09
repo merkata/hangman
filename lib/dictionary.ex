6557c11d3b3600124b758fb7e6be92c36a2bf6eb
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2017-10-09 07:50:25 +0300

    Initial commit

---
+++ lib/dictionary.ex
@@ -0,0 +1,28 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for Dictionary.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dictionary.hello
+      :world
+
+  """
+  def hello do
+    IO.puts "Hello world?!"
+  end
+
+  def random_word do
+    Enum.random(word_list())
+  end
+
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list = String.split(contents, ~r/\n/)
+  end
+
+end
