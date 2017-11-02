d22e4c0b4c9c3a83efb1cd594fd05141d427ff0b
Mariyan Dimitrov <mddimitrov@telenor.bg>
2017-11-02 11:33:44 +0200

    More functional approach in dictionary.ex

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -13,16 +13,18 @@ defmodule Dictionary do
 
   """
   def hello do
-    IO.puts "Hello world?!"
+    :hello
   end
 
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 
   def word_list do
-    contents = File.read!("assets/words.txt")
-    list = String.split(contents, ~r/\n/)
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
 
 end
