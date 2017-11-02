c3bb4d68a0978896f015e4c15189bc1b9c3b36ec
Mariyan Dimitrov <mddimitrov@telenor.bg>
2017-11-02 11:35:46 +0200

    Added credo and dogma as deps

--- mix.exs
+++ mix.exs
@@ -28,6 +28,9 @@ defmodule Dictionary.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dogma, "~> 0.1", only: :dev}
+    ]
   end
 end
